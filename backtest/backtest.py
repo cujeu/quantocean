903c57f568c1137bccf2f85505026033baaecfc7
ubuntu64 <junch39@hotmail.com>
2018-12-06 21:44:54 -0500

    [task] add backtest platform
    Dec/6 clone and make a backtest platform work

---
+++ backtest/backtest.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 10:27:00 2014
+
+@author: colin4567
+"""
+
+import datetime
+import pprint
+import queue
+import time
+
+class Backtest(object):
+    """
+    Encapsulates the settings and components for carrying out an event-driven
+    backtest.
+    """
+    def __init__(self, symbol_list, data_feed, initial_capital,
+                 heartbeat, start_date, data_handler, execution_handler, 
+                 portfolio, strategy, db_host=None, db_user=None, db_pass=None, db_name=None, csv_dir=None):
+        """
+        Initializes the backtest.
+        
+        Parameters:
+        csv_dir - the hard root to the CSV data directory
+        symbol_list - list of symbol string
+        initial_capital - starting capital
+        heartbeat - backtest heartbeat in seconds
+        start_date - starting date of strategy
+        data_handler - (Class) handles the market data feed
+        execution_handler - (Class) handles the orders/fills for trades
+        portfolio - (Class) Keeps track of current and prior positions
+        strategy - (Class) generates signals based on market data
+        """
+        self.symbol_list = symbol_list
+        self.data_feed = data_feed
+        self.initial_capital = initial_capital
+        self.heartbeat = heartbeat
+        self.start_date = start_date
+        self.data_handler_cls = data_handler
+        self.execution_handler_cls = execution_handler
+        self.portfolio_cls = portfolio
+        self.strategy_cls = strategy
+        
+        self.csv_dir = csv_dir
+        self.db_host = db_host
+        self.db_user = db_user
+        self.db_pass = db_pass
+        self.db_name = db_name
+        
+        
+        self.events = queue.Queue()
+        self.signals = 0
+        self.orders = 0
+        self.fills = 0
+        self.num_strats = 1
+        
+        self._generate_trading_instances(data_feed)
+    
+
+    def _generate_trading_instances(self, data_feed):
+        """
+        Generates the trading instance objects from their class types
+        """
+        print ("Creating DataHandler, Strategy, Portfolio, and ExecutionHandler...")
+        
+        if self.data_feed == 1: #HistoricCSVDataHandler
+            self.data_handler = self.data_handler_cls(self.events, self.csv_dir, self.symbol_list, self.start_date)
+        elif self.data_feed == 2: #MySQLDataHandler
+            self.data_handler = self.data_handler_cls(self.events, self.db_host, self.db_user, self.db_pass, self.db_name, self.symbol_list, self.start_date) 
+                                                  
+        self.strategy = self.strategy_cls(self.data_handler, self.events)
+        
+        self.portfolio = self.portfolio_cls(self.data_handler, self.events, 
+                                            self.start_date, self.initial_capital)
+                                            
+        self.execution_handler = self.execution_handler_cls(self.events)
+        
+    
+    def _run_backtest(self):
+        """
+        Executes the backtest. Outer loop keeps track of the heartbeat, 
+        inner checks if there is an event in the Queue object and sacts on it 
+        by calling the appropriate method on the necessary object.
+        """
+        i = 0
+        while True:
+            i += 1
+            #Update the market bars
+            if self.data_handler.continue_backtest == True:
+                self.data_handler.update_bars()
+                self.data_handler.update_symbol_list(min_bars=self.strategy.long_window)
+            else:
+                break
+            
+            #handle the events
+            while True:
+                try:
+                    event = self.events.get(False) #block=False
+                except queue.Empty:
+                    break
+                else:
+                    if event is not None:
+                        if event.type == 'MARKET':
+                            self.strategy.calculate_signals(event)
+                            self.portfolio.update_timeindex(event)
+                        
+                        elif event.type == 'SIGNAL':
+                            self.signals += 1
+                            self.portfolio.update_signal(event)
+                    
+                        elif event.type == 'ORDER':
+                            self.orders += 1
+                            self.execution_handler.execute_order(event)
+                        
+                        elif event.type == 'FILL':
+                            self.fills += 1
+                            self.portfolio.update_fill(event)
+                            
+            time.sleep(self.heartbeat)
+    
+    
+    def _output_performance(self):
+        """
+        Outputs the strategy performance from the backtest.
+        """
+
+        self.portfolio.create_equity_curve_dataframe()
+        self.portfolio.create_positioning_dataframe()
+        
+        print( "Creating summary stats...")
+        stats = self.portfolio.output_summary_stats()
+        
+        print(  "Creating the equity curve...\n")
+        print(  self.portfolio.equity_curve[:50])
+        print('')        
+
+        print(  "Ending equity curve...\n")
+        print(  self.portfolio.equity_curve.tail(10)) 
+        
+        
+        print(  "Creating the historical positioning...\n")
+        print(  self.portfolio.positions.tail(10))
+        print('')
+        pprint.pprint(stats)
+        
+        print('')
+        print("Signals: %s" % self.signals)
+        print("Orders: %s" % self.orders)
+        print("Fills: %s" % self.fills)
+        
+        
+    
+    def simulate_trading(self):
+        """
+        This is called from __main__.
+        Simulates the backtest and outputs portfolio performance
+        """
+        self._run_backtest()
+        self._output_performance()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                        
+                        
+                    
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
---
+++ backtest/mac.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 11:50:53 2014
+
+@author: colin4567
+"""
+
+import numpy as np
+import pandas as pd
+import datetime
+
+from backtest import Backtest
+from data import HistoricCSVDataHandler
+from data import MySQLDataHandler
+from event import SignalEvent
+from execution import SimulatedExecutionHandler
+from portfolio import Portfolio
+from strategy import Strategy
+
+
+class MovingAverageCrossStrategy(Strategy):
+    """
+    Default window is 34/144
+    """
+    def __init__(self, bars, events, short_window=34, long_window=144):
+        """
+        Initializes the buy and hold strategy
+        
+        Parameters:
+        bars - DataHandler object that provides bar info
+        events - Event queue object
+        short/long windows - moving average lookbacks
+        """
+        self.bars = bars
+        self.symbol_list = self.bars.symbol_list
+        self.events = events
+        self.short_window = short_window
+        self.long_window = long_window
+        
+        #set to true if a symbol if in the market
+        self.bought = self._calculate_initial_bought()
+    
+    
+    def _calculate_initial_bought(self):
+        """
+        Adds keys to the bought dictionary for all symbols and sets them to 'OUT'
+        """
+        bought = {}        
+        for s in self.symbol_list:
+            bought[s] = 'OUT'
+        return bought
+    
+    
+    def calculate_signals(self, event):
+        """
+        Generates a new SignalEvent object and places it on the events 
+        Event queue, and updates the bought attribute.
+        
+        Parameters:
+        event - a MarketEvent object
+        
+        To Do:
+        strength needs to be applied to all positions when a new symbol comes on, not just to new position
+        """
+        if event.type == 'MARKET':
+            for symbol in self.bars.latest_symbol_list: 
+                bars = self.bars.get_latest_bars_values(symbol, "adj_close", N=self.long_window)
+                
+                if bars is not None and bars != []:
+                    short_sma = np.mean(bars[-self.short_window:])
+                    long_sma = np.mean(bars[-self.long_window:])
+                    dt = self.bars.get_latest_bar_datetime(symbol)
+                    sig_dir = ""
+                    strength = 1.0 / len(self.bars.latest_symbol_list) #this is where you set percentage of capital - how can I easily rescale previous positions when new symbol becomes eligable
+                    strategy_id = 1
+                    
+                    if short_sma > long_sma and self.bought[symbol] == 'OUT':
+                        sig_dir = 'LONG'
+                        signal = SignalEvent(strategy_id, symbol, dt, sig_dir, strength)
+                        self.events.put(signal)
+                        self.bought[symbol] = 'LONG'
+                    
+                    elif short_sma < long_sma and self.bought[symbol] == 'LONG':
+                        sig_dir = 'EXIT'
+                        signal = SignalEvent(strategy_id, symbol, dt, sig_dir, strength)
+                        self.events.put(signal)
+                        self.bought[symbol] = 'OUT'
+
+
+if __name__ == '__main__':
+    
+    import os
+    
+    ##symbol_list = ['ba', 'noc', 'lmt', 'nwsa', 'goog', 'alle', 'navi'] 
+    symbol_list = ['AAPL']
+    #broken = ['nwsa', 'goog', 'alle', 'navi']
+    #symbols =  pd.read_csv("C:/Users/colin4567/Dropbox/EventTradingEngine/getData/testData/sp500ticks.csv"); symbol_list = symbols['tickers'].tolist()
+    initial_capital = 10000000.0 #10m
+    start_date = datetime.datetime(2013,6,3,0,0,0)
+    heartbeat = 0.0
+    data_feed = 2 # 1 is csv, 2 is MySQL
+    
+    if data_feed == 1:
+        if os.path.isdir("/home/jun/proj/quantocean/eventDriven/testData"):
+            csv_dir = os.path.normpath("/home/jun/proj/quantocean/eventDriven/testData")
+        else:
+            raise SystemExit("No csv dir found ")
+            
+        backtest = Backtest(symbol_list, 
+                            data_feed,
+                            initial_capital, 
+                            heartbeat, 
+                            start_date, 
+                            HistoricCSVDataHandler, 
+                            SimulatedExecutionHandler, 
+                            Portfolio, 
+                            MovingAverageCrossStrategy, 
+                            csv_dir=csv_dir)  
+                        
+    elif data_feed == 2:
+        db_host = 'localhost'; db_user = 'sec_user'; db_pass = 'password'; db_name = 'securities_master';
+        
+        backtest = Backtest(symbol_list, 
+                            data_feed,
+                            initial_capital, 
+                            heartbeat, 
+                            start_date, 
+                            MySQLDataHandler, 
+                            SimulatedExecutionHandler, 
+                            Portfolio, 
+                            MovingAverageCrossStrategy, 
+                            db_host=db_host, db_user=db_user, db_pass=db_pass, db_name=db_name)  
+
+                  
+    
+    backtest.simulate_trading()
+                    
+                    
+                    
+                    
+                    
+                    
